30389aba66bbf823691ff33f1a57a1c5f7f1c4f9
ThapaSurace <zorosenpai456@gmail.com>
2024-08-23 07:55:30 +0545

    updated url

--- e-store/src/utils/newRequest.js
+++ e-store/src/utils/newRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://e-store-pi.vercel.app/api/';
+const BASE_URL = 'https://e-store-tbyv.vercel.app/api/';
 
 export const newRequest = axios.create({
 	baseURL: BASE_URL,
